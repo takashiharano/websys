b883fa5da5fa8c5a7cdd0a029515b83c4c046b66
Takashi Harano <takashiharano@users.noreply.github.com>
2023-02-19 13:33:17 +0900

    Remove unused import

--- bin/authman.py
+++ bin/authman.py
@@ -4,7 +4,6 @@
 #==============================================================================
 import os
 import sys
-import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import websysconf
