bba6ff56f861040e9d349745180e4448f2951616
Takashi Harano <takashiharano@users.noreply.github.com>
2023-07-08 20:46:44 +0900

    Add user state definition

--- websys.js
+++ websys.js
@@ -6,6 +6,11 @@ var websys = {};
 websys.ST_LOGIN_ID = 1;
 websys.ST_LOGIN_PW = 2;
 websys.ST_CHANGE_PW = 3;
+
+websys.U_ST_DISABLED = 1;
+websys.U_ST_LOCKED = 1 << 1;
+websys.U_ST_NEED_PW_CHANGE = 1 << 2;
+
 websys.initStatus = 0;
 websys.status = 0;
 websys.isDbgAvailable = false;
